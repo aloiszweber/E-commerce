521805397ba0cb59e69c2f3cd027101ef4121929
becode <alois.zweber@gmail.com>
2022-06-14 23:08:49 +0200

    Nearly finish with the dynamic display of my products from the json file

--- javascript/basket.js
+++ javascript/basket.js
@@ -44,3 +44,21 @@ function changeQuantity(product, quantity) {
         }
     }
 }
+
+function getNumberProduct() {
+    let basket = getBasket();
+    let number = 0;
+    for (let product of basket) {
+        number += product.quantity;
+    }
+    return number;
+}
+
+function getTotalPrice() {
+    let basket = getBasket();
+    let total = 0;
+    for (let product of basket) {
+        total += product.quantity * product.price;
+    }
+    return total;
+}
