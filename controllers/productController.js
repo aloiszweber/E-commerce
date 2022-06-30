53ef4234b19a4a0823103c004479758873ce1006
Fabien V <fabien.vermeiren@gmail.com>
2022-06-30 09:36:23 +0200

    catchAsync util functions finalizing implementation

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,40 +1,27 @@
 const Product = require("./../models/productModel");
 const catchAsync = require("./../utils/catchAsync");
 //TODO add catchAsync and remove catch from functions
-exports.getAllProducts = async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.status(200).json({
-      status: "success",
-      results: products.length,
-      data: {
-        products,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-exports.getProduct = async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    //could use Product.findOne({id:req.params.id})
-    res.status(200).json({
-      status: "success",
-      data: {
-        product,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: "Invalid data sent!",
-    });
-  }
-};
+//remember to add next to all functions
+exports.getAllProducts = catchAsync(async (req, res, next) => {
+  const products = await Product.find();
+  res.status(200).json({
+    status: "success",
+    results: products.length,
+    data: {
+      products,
+    },
+  });
+});
+exports.getProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  //could use Product.findOne({id:req.params.id})
+  res.status(200).json({
+    status: "success",
+    data: {
+      product,
+    },
+  });
+});
 
 exports.createProduct = catchAsync(async (req, res, next) => {
   const newProduct = await Product.create(req.body);
